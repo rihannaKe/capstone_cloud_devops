6a3b87d7162155535278827c33287e0c74995b63
rihannaKe <rihanna.ke@gmail.com>
2020-06-16 14:33:14 +0200

    parmas change

--- aws/create_eks.sh
+++ aws/create_eks.sh
@@ -1,1 +1,1 @@
-aws cloudformation create-stack --stack-name udacity-capstone-eks --template-body file://eks.yml --parameters file://eks_parameters.json
+aws cloudformation create-stack --stack-name udacity-capstone-eks --template-body file://aws/eks.yml --parameters file://aws/eks_parameters.json
